27a36564fd32c3b90cdd8f049309707e8a073e5f
tcarl44 <tcarlsenkamga@gmail.com>
2023-09-27 12:37:38 +0100

    slider

---
+++ src/Carousle/Carousle.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import './Carousle.css'
+
+const Carousle = () => {
+
+    const getBox = () => {
+        return document.querySelector('.CarousleContainer')
+    };
+
+    const prev = () => {
+        const box = getBox();
+        const width = box.clientWidth;
+        box.scrollLeft = box.scrollLeft - width;
+        // return next();
+    };
+
+    const next = () => {
+        const box = getBox();
+        const width = box.clientWidth;
+        // console.log("next width: " + box.clientWidth)
+        box.scrollLeft = box.scrollLeft + width;
+    };
+
+
+
+  return (
+    <>
+
+    <div className="Carousle">
+    <div className="Review">
+        <h2>Let's see what our clients says</h2>
+    </div>
+   
+        <div className="CarousleContainer">
+
+            <div className="CarousleItem">
+                <div className="List">
+                    <h1>Mikel</h1>
+                    <p>Only natural ingredients, grown and produced in the village , with care and love for out health, love and longevity ! I always choose only you, I have never regretted it!</p>
+                    <p>1</p>
+                </div>
+            </div>
+
+            <div className="CarousleItem">
+                <div className="List">
+                <h1>Bradon</h1>
+                    <p>Only natural ingredients, grown and produced in the village , with care and love for out health, love and longevity ! I always choose only you, I have never regretted it!</p>
+                    <p>2</p>
+                </div>
+            </div>
+
+            <div className="CarousleItem">
+                <div className="List">
+                <h1>Carlsen</h1>
+                    <p>Only natural ingredients, grown and produced in the village , with care and love for out health, love and longevity ! I always choose only you, I have never regretted it!</p>
+                    <p>3</p>
+                </div>
+            </div>
+
+            <div className="CarousleItem">
+                <div className="List">
+                <h1>John</h1>
+                    <p>Only natural ingredients, grown and produced in the village , with care and love for out health, love and longevity ! I always choose only you, I have never regretted it!</p>
+                    <p>4</p>
+                </div>
+            </div>
+            <div className="CarousleItem">
+                <div className="List">
+                <h1>Ronnet</h1>
+                    <p>Only natural ingredients, grown and produced in the village , with care and love for out health, love and longevity ! I always choose only you, I have never regretted it!</p>
+                    <p>5</p>
+                </div>
+            </div>
+        </div>
+        <div>
+           <button className="prev" onClick={prev}>
+                prev
+            </button>
+        <button className="next" onClick={next}>
+            next
+        </button>
+        </div>
+
+    </div>
+    </>
+  )
+};
+
+export default Carousle
